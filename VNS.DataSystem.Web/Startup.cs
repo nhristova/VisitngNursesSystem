bf46b2adcf532a10f7dc0ecc16c481dadaf75e2f
Nadezhda Hristova <epohster@gmail.com>
2017-09-29 18:42:28 +0300

    Created a default MVC project

---
+++ VNS.DataSystem.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VNS.DataSystem.Web.Startup))]
+namespace VNS.DataSystem.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
