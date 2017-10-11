68afb0126eb4e6605710474dd5ae84eb224b64f8
Nadezhda Hristova <epohster@gmail.com>
2017-10-12 00:01:29 +0300

    Initial unit tests #6

--- VNS.Web.Tests/Controllers/HomeControllerTest.cs
+++ VNS.Web.Tests/Controllers/HomeControllerTest.cs
@@ -1,49 +1,54 @@
 ﻿using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using VNS.Web.Controllers;
+using VNS.Web.Controllers;
+using VNS.Services.Contracts;
+using Moq;
 
 namespace VNS.Web.Tests.Controllers
 {
-    //[TestClass]
-    //public class HomeControllerTest
-    //{
-    //    [TestMethod]
-    //    public void Index()
-    //    {
-    //        // Arrange
-    //        HomeController controller = new HomeController();
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            var visitServiceMock = new Mock<IVisitsService>();
+            HomeController controller = new HomeController(visitServiceMock.Object);
 
-    //        // Act
-    //        ViewResult result = controller.Index() as ViewResult;
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
 
-    //        // Assert
-    //        Assert.IsNotNull(result);
-    //    }
+            // Assert
+            Assert.IsNotNull(result);
+        }
 
-    //    [TestMethod]
-    //    public void About()
-    //    {
-    //        // Arrange
-    //        HomeController controller = new HomeController();
+        [TestMethod]
+        public void About()
+        {
+            // Arrange
+            var visitServiceMock = new Mock<IVisitsService>();
+            HomeController controller = new HomeController(visitServiceMock.Object);
 
-    //        // Act
-    //        ViewResult result = controller.About() as ViewResult;
+            // Act
+            ViewResult result = controller.About() as ViewResult;
 
-    //        // Assert
-    //        Assert.AreEqual("Your application description page.", result.ViewBag.Message);
-    //    }
+            // Assert
+            Assert.AreEqual("Helping nurses help families with babies.", result.ViewBag.Message);
+        }
 
-    //    [TestMethod]
-    //    public void Contact()
-    //    {
-    //        // Arrange
-    //        HomeController controller = new HomeController();
+        [TestMethod]
+        public void Help()
+        {
+            // Arrange
+            var visitServiceMock = new Mock<IVisitsService>();
+            HomeController controller = new HomeController(visitServiceMock.Object);
 
-    //        // Act
-    //        ViewResult result = controller.Contact() as ViewResult;
+            // Act
+            ViewResult result = controller.Help() as ViewResult;
 
-    //        // Assert
-    //        Assert.IsNotNull(result);
-    //    }
-    //}
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
 }
